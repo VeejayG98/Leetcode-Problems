4b6b627f063d8df20de5a1144bcfcbfb70684546
Jayasuryaa Govindraj <69819182+VeejayG98@users.noreply.github.com>
2022-08-30 18:18:28 -0500

    Time: 251 ms (36.10%), Space: 15.2 MB (79.01%) - LeetHub

---
+++ 268-missing-number/268-missing-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        ans = [False] *(len(nums) + 1)
+        for num in nums:
+            ans[num] = True
+        print(ans)
+        for res in range(len(ans)):
+            if ans[res] == False:
+                return res
