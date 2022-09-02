e34de501999ffa4797a4b588e36f55e8e80201e2
Jayasuryaa Govindraj <69819182+VeejayG98@users.noreply.github.com>
2022-09-02 13:52:09 -0500

    Time: 591 ms (46.23%), Space: 14.3 MB (78.18%) - LeetHub

--- 487-max-consecutive-ones-ii/487-max-consecutive-ones-ii.py
+++ 487-max-consecutive-ones-ii/487-max-consecutive-ones-ii.py
@@ -4,15 +4,15 @@ class Solution:
         j = 0
         zerosCount = 0
         maxCount = 0
-        
+
         while j < len(nums):
             if nums[j] == 0:
                 zerosCount += 1
-                
-                while zerosCount > 1:
-                    if nums[i] == 0:
-                        zerosCount -= 1
-                    i += 1
-                    maxCount = max(maxCount, j - i + 1)
+
+            while zerosCount > 1:
+                if nums[i] == 0:
+                    zerosCount -= 1
+                i += 1
+            maxCount = max(maxCount, j - i + 1)
             j += 1
         return max(maxCount, j - i)
