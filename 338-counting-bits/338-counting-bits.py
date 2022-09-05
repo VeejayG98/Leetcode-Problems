621805ba85792e22eff7f5d8f7a52a58199632b6
Jayasuryaa Govindraj <69819182+VeejayG98@users.noreply.github.com>
2022-09-05 15:31:23 -0500

    Time: 514 ms (5.00%), Space: 20.7 MB (79.02%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,17 +1,15 @@
 class Solution:
-    
-    def hammingWeight(self, n):
-        
-        count = 0
-        while n != 0:
-            n = n & (n - 1)
-            count += 1
-        return count
-    
     def countBits(self, n: int) -> List[int]:
-        
         ans = []
         for i in range(n + 1):
             ans.append(self.hammingWeight(i))
         return ans
     
+    def hammingWeight(self, num):
+        mask = 1
+        count = 0
+        for i in range(32):
+            if num & mask != 0:
+                count += 1
+            mask <<= 1
+        return count
