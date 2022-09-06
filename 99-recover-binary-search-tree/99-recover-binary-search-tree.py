7b3bce1a34f8f0e76e870c1d760de421342995e7
Jayasuryaa Govindraj <69819182+VeejayG98@users.noreply.github.com>
2022-09-06 17:43:34 -0500

    Time: 135 ms (25.86%), Space: 14.4 MB (26.00%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -5,30 +5,28 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.x = None
+        self.y = None
+        self.prev = None
     def recoverTree(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        stack = []
-        self.inOrder(root, stack)
-        node1, node2 = self.findSwap(stack)
-        node1.val, node2.val = node2.val, node1.val
-    
-    def findSwap(self, stack):
-        firstOccurance = False
-        for i in range(len(stack) - 1):
-            if stack[i].val > stack[i + 1].val:
-                if firstOccurance:
-                    num2 = stack[i + 1]
-                    break
-                firstOccurance = True
-                num1 = stack[i]
-                num2 = stack[i + 1]
-        return num1, num2
+        self.findTwoSwapped(root)
+        self.x.val, self.y.val = self.y.val, self.x.val
         
-    def inOrder(self, node, stack):
+    
+    def findTwoSwapped(self, node):
         if node is None:
             return
-        self.inOrder(node.left, stack)
-        stack.append(node)
-        self.inOrder(node.right, stack)
+        self.findTwoSwapped(node.left)
+        if self.prev and self.prev.val > node.val:
+            self.y = node
+            
+            if self.x is None:
+                self.x = self.prev
+        self.prev = node
+        self.findTwoSwapped(node.right)
+        
+        
