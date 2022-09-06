24abbd117dba21240899d4d96f6bcf57c5e4190f
Jayasuryaa Govindraj <69819182+VeejayG98@users.noreply.github.com>
2022-09-06 13:55:58 -0500

    Time: 101 ms (24.19%), Space: 13.6 MB (99.91%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -20,27 +20,27 @@ class Solution:
             length = len(queue)
             for k in range(length):
                 i, j = queue.pop(0)
-                if grid[i][j] == 2:
-                    grid[i][j] = 0
+                # if grid[i][j] == 2:
+                #     grid[i][j] = 0
                             
-                    if i - 1 >= 0 and grid[i - 1][j] == 1:
-                        grid[i - 1][j] = 2
-                        queue.append((i - 1, j))
-                        fresh_oranges -= 1
+                if i - 1 >= 0 and grid[i - 1][j] == 1:
+                    grid[i - 1][j] = 2
+                    queue.append((i - 1, j))
+                    fresh_oranges -= 1
 
-                    if i + 1 < len(grid) and grid[i + 1][j] == 1:
-                        grid[i + 1][j] = 2
-                        queue.append((i + 1, j))
-                        fresh_oranges -= 1
+                if i + 1 < len(grid) and grid[i + 1][j] == 1:
+                    grid[i + 1][j] = 2
+                    queue.append((i + 1, j))
+                    fresh_oranges -= 1
 
-                    if j - 1 >= 0 and grid[i][j - 1] == 1:
-                        grid[i][j - 1] = 2
-                        queue.append((i, j - 1))
-                        fresh_oranges -= 1
+                if j - 1 >= 0 and grid[i][j - 1] == 1:
+                    grid[i][j - 1] = 2
+                    queue.append((i, j - 1))
+                    fresh_oranges -= 1
 
-                    if j + 1 < len(grid[0]) and grid[i][j + 1] == 1:
-                        grid[i][j + 1] = 2
-                        queue.append((i, j + 1))
-                        fresh_oranges -= 1
+                if j + 1 < len(grid[0]) and grid[i][j + 1] == 1:
+                    grid[i][j + 1] = 2
+                    queue.append((i, j + 1))
+                    fresh_oranges -= 1
             time += 1
         return time if fresh_oranges == 0 else -1
