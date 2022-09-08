13071abc88e3757ab4e2d130d86e45f3c875e841
Jayasuryaa Govindraj <69819182+VeejayG98@users.noreply.github.com>
2022-09-08 15:27:10 -0500

    Time: 2930 ms (28.73%), Space: 32.5 MB (10.51%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,38 +1,17 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-#         print(set(nums))
-#         nums = list(set(nums))
-#         print(nums)
-#         count = 1
-#         maxCount = 0
-#         for i in range(1, len(nums)):
-#             if nums[i] == nums[i - 1] + 1:
-#                 count += 1
-#                 print(count, nums[i])
-#             else:
-#                 maxCount = max(maxCount, count)
-        
-#         return max(maxCount, count)
-        
-            
-        
-        # print(sorted(nums))
-        # print(set(nums))
+
         longest_seq = {}
-        largest_num = -math.inf
         for num in nums:
-            # print(num)
             if num in longest_seq:
                 continue
             if num - 1 in longest_seq and num + 1 in longest_seq:
                 longest_seq[num] = longest_seq[num + 1] + 1
-                # longest_seq[num - 1] = longest_seq[num] + 1
                 while num - 1 in longest_seq:
                     longest_seq[num - 1] = longest_seq[num] + 1
                     num = num - 1
             elif num - 1 in longest_seq:
                 longest_seq[num] = longest_seq[num - 1]
-                # longest_seq[num - 1] += 1
                 while num - 1 in longest_seq:
                     longest_seq[num - 1] += 1
                     num = num - 1
@@ -40,8 +19,4 @@ class Solution:
                 longest_seq[num] = longest_seq[num + 1] + 1
             else:
                 longest_seq[num] = 1
-            largest_num = max(largest_num, num)
-            # print(longest_seq)
-            # print(sorted(longest_seq.keys()))
-        # print(max(longest_seq.values()))
         return max(longest_seq.values()) if longest_seq else 0
