35625c7915dd8b7b89377d792065bd713f148bf0
Jayasuryaa Govindraj <69819182+VeejayG98@users.noreply.github.com>
2022-09-09 13:12:25 -0500

    Time: 315 ms (5.03%), Space: 18.5 MB (42.11%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
         word_dict = defaultdict(list)
         for word in strs:
-            alphabets = [0] * 26
-            for letter in word:
-                alphabets[ord(letter) - ord('a')] += 1
-            word_dict[tuple(alphabets)].append(word)
+            key = tuple(sorted(word))
+            word_dict[key].append(word)
+        
         return word_dict.values()
