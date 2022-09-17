613a2b9fd84f48f6c4296b656b1abe21ba698ae2
Jayasuryaa Govindraj <69819182+VeejayG98@users.noreply.github.com>
2022-09-17 13:06:20 -0500

    Time: 212 ms (26.55%), Space: 18.7 MB (71.69%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -3,4 +3,4 @@ class Solution:
         freq = {}
         for num in nums:
             freq[num] = freq.get(num, 0) + 1
-        return heapq.nlargest(k, freq.keys(), key = lambda x: freq.get(x))
+        return heapq.nlargest(k, freq.keys(), freq.get)
