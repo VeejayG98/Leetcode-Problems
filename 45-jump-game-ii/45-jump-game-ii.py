6f8e8a48c1b3311ef24bea687f72feee654bafb7
Jayasuryaa Govindraj <69819182+VeejayG98@users.noreply.github.com>
2022-09-18 14:04:51 -0500

    Time: 269 ms (46.67%), Space: 15.2 MB (21.90%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,15 +1,16 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        i = len(nums) - 2
-        goal = len(nums) - 1
-        stepsTaken = 0
-        while i >= 0 and goal != 0:
-            furthest_step_backwards = goal
-            for j in range(i, -1, -1):
-                if j + nums[j] >= goal:
-                    furthest_step_backwards = min(furthest_step_backwards, j)
-            goal = furthest_step_backwards
-            stepsTaken += 1
-            i = goal - 1
+        currentJump = 0
+        start = 0
+        furthestJump = 0
+        jumps = 0
+        
+        for i in range(len(nums) - 1):
             
-        return stepsTaken
+            currentJump = max(currentJump, nums[i] + i)
+            if i == furthestJump:
+                jumps += 1
+                furthestJump = currentJump
+        
+        return jumps
+            
