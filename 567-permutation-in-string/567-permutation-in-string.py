d13c05555f5d7ded6a72755ea8d38c2702ad539d
Jayasuryaa Govindraj <69819182+VeejayG98@users.noreply.github.com>
2022-09-26 19:29:22 -0500

    Time: 144 ms (54.61%), Space: 13.9 MB (68.10%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,24 +1,16 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         key_s1 = [0] * 26
-        freq = {}
         for letter in s1:
             key_s1[ord(letter) - ord('a')] += 1
-            freq[letter] = freq.get(letter, 0) + 1
         key_s2 = [0] * 26
-        i = 0
-        j = 0
-        while i < len(s2) and j < len(s2):
-            letter = s2[j]
+        for i in range(len(s2)):
+            letter = s2[i]
             key_s2[ord(letter) - ord('a')] += 1
             
-            if letter not in freq or freq[letter] == 0:
-                key_s2[ord(s2[i]) - ord('a')] -= 1
-                i += 1
-            else:
-                freq[letter] -= 1
+            if i >= len(s1):
+                key_s2[ord(s2[i - len(s1)]) - ord('a')] -= 1
             
             if key_s1 == key_s2:
                 return True
-            j += 1
         return False
